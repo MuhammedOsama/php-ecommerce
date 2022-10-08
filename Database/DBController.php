e995ad1f2e1b1bc50358b4a6b42821d5f72ef6ad
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2022-10-08 13:48:19 +0200

    Updated Database

--- Database/DBController.php
+++ Database/DBController.php
@@ -6,7 +6,7 @@ class DBController
     protected $host = "localhost";
     protected $user = "root";
     protected $password = "";
-    protected $dbName = "php_ecommerce";
+    protected $dbName = "ecommerce_php";
 
     //Connection Property
     public $conn = null;
